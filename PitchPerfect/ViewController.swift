0bb80502cbcfefa05e9adfb65de51ded34ce71bf
Robert Racz <robertbracz@gmail.com>
2017-01-29 23:40:41 -0600

    xcassets, button actions

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,13 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordingLabel: UILabel!    
+    @IBOutlet weak var recordingButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        stopRecordingButton.isEnabled = false;
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -24,11 +33,18 @@ class ViewController: UIViewController {
 
     @IBAction func recordAutio(_ sender: Any) {
         print("record button was pressed")
+        
         recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true;
+        recordingButton.isEnabled = false;
     }
 
     @IBAction func stopRecording(_ sender: Any) {
         print("stop recording button was pressed")
+        
+        recordingLabel.text = "Tap to Record"
+        stopRecordingButton.isEnabled = false;
+        recordingButton.isEnabled = true;
     }
 }
 
