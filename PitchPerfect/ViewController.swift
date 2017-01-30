cae9a6ceb103aa06a857676f46406c7b9f781823
Robert Racz <robertbracz@gmail.com>
2017-01-29 22:56:13 -0600

    lesson 2, first storyboard, constraints, inactions iboutlets

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAutio(_ sender: Any) {
+        print("record button was pressed")
+        recordingLabel.text = "Recording in Progress"
+    }
 
+    @IBAction func stopRecording(_ sender: Any) {
+        print("stop recording button was pressed")
+    }
 }
 
